bca329291edfcebbce0754ebd1b5fde8d0e5bcb1
Dan Woodson <woodson.dan@gmail.com>
2012-08-13 21:54:12 -0500

    Big push...

--- library.js
+++ library.js
@@ -15,9 +15,7 @@ var obj = {"artists":[]};
  *   the artist object/undefined for exists/does not exist
  */
 this.artist_exist = function (artist){
-  // Search for artist
-  //console.log('In artist_exist, returning:');
-  //console.dir(_.find(obj.artists, function(elm){return elm.artist == artist;}));
+  // Return artist object (or undefined)
   return _.find(obj.artists, function(elm){return elm.artist == artist;});
 };
 
@@ -34,14 +32,12 @@ this.artist_exist = function (artist){
  *  Returns:
  *   album_obj/undefined for exists/does not exist
  */
-/*NOT CURRENTLY USED*/
 this.album_exist = function (album_obj){
-//console.log('obj');
-//console.dir(obj);
-  var artist_obj = this.artist_exist(album_obj.artist) || this.add_artist(album_obj.artist); // index
-//console.log('artist_obj:');
-//console.dir(artist_obj);
-  var artist_idx = (_.indexOf(obj.artists, artist_obj));
+  // Variables
+  var artist_obj = this.artist_exist(album_obj.artist) || this.add_artist(album_obj.artist),  // artist object
+      artist_idx = (_.indexOf(obj.artists, artist_obj));                                      // atrist index
+
+  // Return the album object (or undefined)
   return _.find(obj.artists[artist_idx].albums, function(elm){return elm.album == album;});
 };
 
@@ -56,10 +52,14 @@ this.album_exist = function (album_obj){
  *   artist object/false for added/already exists in library
  */
 this.add_artist = function(artist){
+  // If the artist doesn't already exist
   if(!this.artist_exist(artist)){
+    // Add the arttist
     obj.artists.push({"artist":artist, "albums":[]});
+    // And return it
     return {"artist":artist, "albums":[]};
   }
+  // Else, return false
   return false;
 }
 
@@ -77,6 +77,7 @@ this.add_artist = function(artist){
  *   album object/false for added/already exists in library
  */
 this.add_album = function(album_obj){
+  // Variables
   var artist_obj = this.artist_exist(album_obj.artist) || this.add_artist(album_obj.artist),
       artist_idx = (_.indexOf(obj.artists, artist_obj)),
       album_exists = _.find(obj.artists[artist_idx].albums, function(elm){return elm.album == album_obj.album;});
@@ -84,7 +85,6 @@ this.add_album = function(album_obj){
   // If album does not exists, confirm album_obj has the proper parameters, then add it and return
   if(!album_exists){
     album_obj.songs = [];
-    //console.log('adding '+album_obj.album+' to '+album_obj.artist);
     obj.artists[artist_idx].albums.push(album_obj);
     return album_obj;
   }
@@ -105,34 +105,35 @@ this.add_album = function(album_obj){
  *   album object/false for added/already exists in library
  */
 this.add_song = function(song_obj){
-//console.log('song obj');
-//console.dir(song_obj);
-  var artist_obj = this.artist_exist(song_obj.artist) || this.add_artist(song_obj.artist);
-//console.log('artist obj');
-//console.dir(artist_obj);
-  var artist_idx = _.indexOf(obj.artists, artist_obj);
-//console.log('artist index');
-//console.dir(artist_idx);
-  var album_obj = _.find(obj.artists[artist_idx].albums, function(elm){return elm.album == song_obj.album;});
-//console.log('album_obj');
-//console.dir(album_obj);
-  var album_idx = _.indexOf(obj.artists[artist_idx].albums, album_obj);
-//console.log('album index');
-//console.dir(album_idx);
-  var song_exists = _.find(obj.artists[artist_idx].albums[album_idx].songs, function(elm){return elm.song == song_obj.title;});
+  var artist_obj = this.artist_exist(song_obj.artist) || this.add_artist(song_obj.artist),
+      artist_idx = _.indexOf(obj.artists, artist_obj),
+      album_obj = _.find(obj.artists[artist_idx].albums, function(elm){return elm.album == song_obj.album;}),
+      album_idx = _.indexOf(obj.artists[artist_idx].albums, album_obj),
+      song_exists = _.find(obj.artists[artist_idx].albums[album_idx].songs, function(elm){return elm.song == song_obj.title;});
 
-  // If album does not exists, confirm album_obj has the proper parameters, then add it and return
+  // If song does not exists, add it and return the song object
   if(!song_exists){
-    //console.log('adding '+song_obj.song+' to '+album_obj.artist);
     obj.artists[artist_idx].albums[album_idx].songs.push(song_obj);
     return song_obj;
   }
+  // Else, return false
   return false;
+}
+
+/* Function: get
+ *
+ *  Overwriting of console.log(), returns an indented, stringified version on obj
+ *
+ *  Returns:
+ *   the library
+ */
+this.get = function(){
+  return obj;
 }
 
 /* Function: inspect
  *
- *  If artists does not exist in library, adds artist to library
+ *  Overwriting of console.log(), returns an indented, stringified version on obj
  *
  *  Returns:
  *   the library
--- playlist.js
+++ playlist.js
@@ -1,10 +1,28 @@
 #!/usr/bin/env node
 
-var fs = require('fs'),
-    ID3 = require('id3'),
+var fs      = require('fs'),
+    ID3     = require('id3'),
     library = require('./library'),
-    _ = require('underscore'),
-    util = require('util');
+    _       = require('underscore'),
+    util    = require('util'),
+    http    = require('http'),
+    director= require('director'),
+    plates  = require('plates');
+
+/*** Functions ***/
+/* Function: createHtml
+ *
+ *  Creates html for client
+ *
+ *  Parameters:
+ *   lib_obj: a full library object
+ *
+ *  Returns:
+ *   a string of html
+ */
+var createHtml = function(lib_obj){
+
+};
 
 /* Function: parseFile
  *
@@ -63,26 +81,56 @@ var parseFile = function(file){
  *   currentPath - path to traverse
  */
 var scanFiles = function (currentPath) {
-  //console.log(currentPath);
-  var files = fs.readdirSync(currentPath),
-      id3Obj = {};
+  // Variables
+  var files = fs.readdirSync(currentPath),  // the current path
+      id3Obj = {};                          // id3 data
+
+  // Loop through the files/directories in tthe current path
   for (var i in files) {
-    var currentFile = currentPath + '/' + files[i];
-    var stats = fs.statSync(currentFile);
+    // Local variables
+    var currentFile = currentPath + '/' + files[i], // the path to the current file
+        stats = fs.statSync(currentFile);           // the data for the current file
+
+    // If the file is actually a file and it ends in .mp3
     if (stats.isFile() && /mp3$/.test(currentFile)) {
+      // Get the id3 data from the file
       id3Obj = parseFile(currentFile);
-      //console.log('Adding '+id3Obj.artist+' to library');
+      // Add the url to the id3 data
+      id3Obj.url = currentFile;
+      // Add the artist
       library.add_artist(id3Obj.artist);
-      //console.log('Adding '+id3Obj.album+' to library');
+      // Add the album
       library.add_album({"artist":id3Obj.artist, "album":id3Obj.album});
-      //console.log(parseFile(currentFile));
+      // Add the song
       library.add_song(id3Obj);
     }
+    // Else if the file is actually a directory
     else if (stats.isDirectory()) {
+      // Recursively call scanFiles
       scanFiles(currentFile);
     }
   }
 };
 
+/*** Operations ***/
+// Recursively scan the files for the given directory, or use the one we're currently in
 scanFiles(process.argv[2] || '..');
-console.log(library);
+
+// Create template
+//var artist_list = "<% _.each(artists, function(elm) { %> <div><%= elm.artist %></div> <% }); %>";
+//var album_list = "<% _.each(albums, function(elm) { %> <div><%= elm.album %></div> <% }); %>";
+//var album_list = "<% _.each(songs, function(elm) { %> <div><%= elm.song %></div> <% }); %>";
+
+// Start Server
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  //var artist_tmpl = _.template(artist_list, {artists: library.get().artists});
+  //var album_tmpl = _.template(album_list, {albums: library.get().artists[0].albums});
+  //var song_tmpl = _.template(song_list, {songs: library.get().artists[0].albums});
+  res.end(artist_tmpl+album_tmpl);
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+console.log('lib = ');
+console.log(JSON.stringify(library.get()));
+//console.log(_.template(list, {artists: library.get().artists}));
+
