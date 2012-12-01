a263a3383bc7692550254a885b8aee8390d7157b
Dan Woodson <woodson.dan@gmail.com>
2012-11-30 22:34:38 -0600

    Refactored api.js and updated it's docs. Refs #13

--- client/js/api.js
+++ client/js/api.js
@@ -1,48 +1,47 @@
 // API module
 var API = function () {
 
-  this.getArtists = function(requestObj, callback){
-    $.getJSON('/artist', function(data){
-      if(requestObj){
-        data = _.where(data.artist, requestObj);
-      }
-      //console.log(data):
-      if(callback){
-        callback(data);
-      }
-    });
-  };
-  this.getAlbums = function(requestObj, callback){
-    $.getJSON('/artist/'+requestObj.artist_id+'/album', function(data){
-      // Restful doesn't always send back the correct data
-      data = _.where(data.album, requestObj);
-      if(callback){
-        callback(data);
-      }
-    });
-  };
-  this.getSongs = function(requestObj, callback){
-    console.log(requestObj);
-    if(requestObj.id && localStorage.getItem(requestObj.id)){
+  // Default api call, used by all the wrapper functions
+  var defaultCall = function(url, requestObj, type, callback){ 
+    // Check in localStorage first
+    if(requestObj && requestObj.id && localStorage.getItem(requestObj.id)){
+      // If found in localStorage, convert stringifed object to object return data
       data = JSON.parse(localStorage.getItem(requestObj.id)); 
       if(callback){
         callback(data);
       }
     }
+    // If it's not in localStorage, request it from the server
     else{
-      $.getJSON('/song', function(data){
-        // Restful doesn't always send back the correct data
-        data = _.where(data.song, requestObj);
-        // If looking for a particular id, and single item found, return the single item instead of array
-        if(requestObj.id && data.length === 1){
-          data = data[0];
-          localStorage.setItem(requestObj.id, JSON.stringify(data));
+      $.getJSON(url, function(data){
+        // Clean/filter data
+        if(requestObj){
+          // Restful doesn't always send back the correct data
+          data = _.where(data[type], requestObj);
+          // If looking for a particular id, and single item found, return the single item instead of array
+          if(requestObj.id && data.length === 1){
+            data = data[0];
+            // Save the item to localStorage to reduce request to the server
+            localStorage.setItem(requestObj.id, JSON.stringify(data));
+          }
         }
+        // return data
         if(callback){
           callback(data);
         }
       });
     }
+  };
+
+  // Wrapper functions
+  this.getArtists = function(requestObj, callback){
+    defaultCall('/artist', requestObj, 'artist', callback);
+  };
+  this.getAlbums = function(requestObj, callback){
+    defaultCall('/artist/'+requestObj.artist_id+'/album', requestObj, 'album', callback);
+  };
+  this.getSongs = function(requestObj, callback){
+    defaultCall('/song', requestObj, 'song', callback);
   };
 
 };
