97cc80252ac20b96c1cdd4e7cccaac45e4bdc948
Dan Woodson <woodson.dan@gmail.com>
2012-12-29 21:14:13 -0600

    Added play/pause functionality. Refs #10

--- client/js/player.js
+++ client/js/player.js
@@ -4,95 +4,135 @@ var Player = function(elm){
   // Variables
   var self = this;
   self.elm = elm;
+  self.$elm = $(elm);
+  self.paused = false;
 
-    // Play song. If song passed in, play that. Otherwise, request next song 
-    // from playlist and play that.
-    this.play = function(song){
-      // If song not passed in, get next song from playlist 
-      // and call this function with that song.
-      if(!song){
+  // Play song. If song passed in, play that. Otherwise, decide whether to play, 
+  // pause, or request next song from playlist and play that.
+  this.play = function(song){
+    // If song not passed in, check if the player is currently playing. 
+    // If it is, pause playback. Else if no song passed in and nothing is playing, 
+    // check to see if the player is currently paused. If it is, restart playback.
+    // If no song passed in and the player is not playing and is not paused, get 
+    // the next song from playlist and call this function again with that song.
+    if(!song){
+      if(self.paused){
+        // Restart playback
+        document.getElementsByTagName('audio')[0].play();
+    
+        // Update player element
+        self.$elm.addClass('playing');
+        
+        // Update player object
+        self.paused = false;
+      }
+      else if(document.getElementsByTagName('audio')[0].played.length === 1){
+        self.pause();
+      }
+      else{
+        // Get next song from playlist
         playlist.getNextSong(function(song){
           self.play(song);
         });
       }
-      // If song passed in, remove current audio souce, 
-      // add new audio souce, restart player, and update playlist
-      else{
-        // Remove current audio source
-        $('audio source').remove();
+    }
+    // If song passed in, remove current audio souce, 
+    // add new audio souce, restart player, and update playlist and player object
+    else{
+      // Remove current audio source
+      $('audio source').remove();
 
-        // Loop through song urls and add them to the audio's source
-        for(source in song.urls){
-          var sourceElm = $('<source src="'+song.urls[source].url+
-                          '" type="audio/'+song.urls[source].format+'" />');
-          $('audio').append(sourceElm);
-        }
-
-        // Update player display
-        var titleElm = $(self.elm).find('.title');
-        titleElm.find('.song').text(song.name);
-        titleElm.find('.album').text(song.album.name);
-        titleElm.find('.artist').text(song.artist.name);
-
-        // Restart audio
-        document.getElementsByTagName('audio')[0].pause();
-        document.getElementsByTagName('audio')[0].load();
-        document.getElementsByTagName('audio')[0].play();
-        
-        // Update playlist
-        $('#'+song.elm_id).siblings('.song').removeClass('playing');
-        $('#'+song.elm_id).addClass('playing');
+      // Loop through song urls and add them to the audio's source
+      for(source in song.urls){
+        var sourceElm = $('<source src="'+song.urls[source].url+
+                        '" type="audio/'+song.urls[source].format+'" />');
+        $('audio').append(sourceElm);
       }
-    };
 
-    // Play the previous track (defaults to first track)
-    this.playPrev = function(){
-      playlist.getPrevSong(function(song){
-        self.play(song);
-      });
-    };
+      // Update player track object
+      self.curTrack = song;
 
-    // Play the next track (defaults to first track)
-    this.playNext = function(){
-      playlist.getNextSong(function(song){
-        self.play(song);
-      });
-    };
+      // Update player element
+      var titleElm = $(self.elm).find('.title');
+      titleElm.find('.song').text(song.name);
+      titleElm.find('.album').text(song.album.name);
+      titleElm.find('.artist').text(song.artist.name);
+      self.$elm.addClass('playing');
 
-    // Create player element
-    var createPlayerElm = function(){
-      // Create elements
-      var containerElm = $('<div />'),
-          audioElm = $('<audio controls autoplay style="display:none;"/>'),
-          titleElm = $('<div class="title" />'),
-          songNameElm = $('<span class="song" />'),
-          albumNameElm = $('<span class="album" />'),
-          artistNameElm = $('<span class="artist" />'),
-          controlsElm = $('<div class="controls" />'),
-          playBtn = $('<button title="play" class="play icon"></button>').bind('click', function(){self.play();}),
-          prevBtn = $('<button title="previous" class="prev icon"></button>').bind('click', function(){self.playPrev();}),
-          nextBtn = $('<button title="next" class="next icon"></button>').bind('click', function(){self.playNext();});
-      // Add title elements
-      titleElm.append(songNameElm)
-              .append(albumNameElm)
-              .append(artistNameElm);
-      // Add control elements
-      controlsElm.append(prevBtn)
-                 .append(playBtn)
-                 .append(nextBtn);
-      // Add all elements to container
-      return containerElm.append(titleElm)
-                         .append(audioElm)
-                         .append(controlsElm);
-    };
+      // Restart audio
+      document.getElementsByTagName('audio')[0].pause();
+      document.getElementsByTagName('audio')[0].load();
+      document.getElementsByTagName('audio')[0].play();
+      
+      // Update playlist
+      $('#'+song.elm_id).siblings('.song').removeClass('playing');
+      $('#'+song.elm_id).addClass('playing');
 
-    // Init
-    var init = function(){
-      var playerElm = createPlayerElm();
-      $(self.elm).append(playerElm);
-    };
+      // Update player object
+      self.paused = false;
+    }
+  };
 
-    init();
+  // Play the previous track (defaults to first track)
+  this.playPrev = function(){
+    playlist.getPrevSong(function(song){
+      self.play(song);
+    });
+  };
+
+  // Play the next track (defaults to first track)
+  this.playNext = function(){
+    playlist.getNextSong(function(song){
+      self.play(song);
+    });
+  };
+
+  // Pause playback
+  this.pause = function(){
+    // Pause playback
+    document.getElementsByTagName('audio')[0].pause();
+    
+    // Update player object
+    self.paused = true;
+
+    // Update player element
+    self.$elm.removeClass('playing');
+  };
+
+  // Create player element
+  var createPlayerElm = function(){
+    // Create elements
+    var containerElm = $('<div />'),
+        audioElm = $('<audio controls autoplay style="display:none;"/>'),
+        titleElm = $('<div class="title" />'),
+        songNameElm = $('<span class="song" />'),
+        albumNameElm = $('<span class="album" />'),
+        artistNameElm = $('<span class="artist" />'),
+        controlsElm = $('<div class="controls" />'),
+        playBtn = $('<button title="play" class="play icon"></button>').bind('click', function(){self.play();}),
+        prevBtn = $('<button title="previous" class="prev icon"></button>').bind('click', function(){self.playPrev();}),
+        nextBtn = $('<button title="next" class="next icon"></button>').bind('click', function(){self.playNext();});
+    // Add title elements
+    titleElm.append(songNameElm)
+            .append(albumNameElm)
+            .append(artistNameElm);
+    // Add control elements
+    controlsElm.append(prevBtn)
+               .append(playBtn)
+               .append(nextBtn);
+    // Add all elements to container
+    return containerElm.append(titleElm)
+                       .append(audioElm)
+                       .append(controlsElm);
+  };
+
+  // Init
+  var init = function(){
+    var playerElm = createPlayerElm();
+    $(self.elm).append(playerElm);
+  };
+
+  init();
 };
 
 var player = new Player('section#player');
